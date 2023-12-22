d80a0d00965cf696177415c573e917657be4a903
muuminbaargeele <muuminbaargeele1@gmail.com>
2023-12-22 11:32:58 +0300

    new update

--- frontend/src/lib/apiConfig.js
+++ frontend/src/lib/apiConfig.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'https://albasrawie.com/salonsys'
+// export const API_BASE_URL = 'https://albasrawie.com/salonsys'
+export const API_BASE_URL = 'https://salonsys.000webhostapp.com'
