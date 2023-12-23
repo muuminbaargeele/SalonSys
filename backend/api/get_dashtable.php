f3d8a0c63fd9d0c1a86fc0ce616837c562dd3193
muuminbaargeele <muuminbaargeele1@gmail.com>
2023-12-23 18:53:58 +0300

    Muumin's Last Commit

--- backend/api/get_dashtable.php
+++ backend/api/get_dashtable.php
@@ -30,10 +30,19 @@ if (mysqli_num_rows($get_admin) > 0) {
             }
         }
     } else {
-        $get_SalonUser = mysqli_query($conn, "SELECT r.ReqId, c.CustomerName, c.CustomerPhone, se.Title, se.SubTitle, se.Price, se.ServiceImage, r.QueNO, r.ArrivalTime, r.Status FROM `Requests` as r JOIN `Customers` as c JOIN `Services` as se JOIN `Salons` as s JOIN Admins as a WHERE r.CusID = c.CusID AND r.ServiceID = se.serviceId AND s.SalonID = se.SalonID AND s.SalonID = a.SalonID AND a.SalonID = '$SalonID' AND a.Username = '$Username'");
+        $get_SalonUser = mysqli_query($conn, "SELECT r.ReqId, c.CustomerName, c.CustomerPhone, se.Title, se.SubTitle, se.Price, se.ServiceImage, r.QueNO, r.ArrivalTime, r.Status FROM `Requests` as r JOIN `Customers` as c ON r.CusID = c.CusID JOIN `Services` as se ON r.ServiceID = se.serviceId JOIN `Salons` as s ON s.SalonID = se.SalonID JOIN Admins as a ON s.SalonID = a.SalonID WHERE a.SalonID = '$SalonID' AND a.Username = '$Username'");
+
+        $rows = array();
         while ($fetch = mysqli_fetch_assoc($get_SalonUser)) {
+            // Format the price as a string with a dollar sign
+            $fetch['Price'] = '$' . number_format($fetch['Price'], 2); // Formats the price as $X.XX
+
+            // Add the formatted fetch array to your rows
             $rows[] = $fetch;
         }
+
+        // Now $rows contains all the formatted data
+
     }
     print json_encode($rows);
 } else {
--- backend/api/get_services.php
+++ backend/api/get_services.php
@@ -17,11 +17,21 @@ if (mysqli_num_rows($get_admin) > 0) {
         $fetch = mysqli_fetch_assoc($get_admin);
         $role = $fetch['Role'];
         if ($role != 'MainAdmin') {
-            $SalonID = $fetch['SalonID'];
+            $SalonID = $fetch['SalonID'];  // Assuming $fetch['SalonID'] is previously defined and contains the correct SalonID
+
             $get_Services = mysqli_query($conn, "SELECT se.serviceId, se.Title, se.SubTitle, se.CreateDT, se.ServiceImage, se.Price FROM `Services` se JOIN `Salons` s ON se.SalonID = s.SalonID WHERE s.SalonID = '$SalonID'");
+
+            $rows = array();
             while ($fetch_Services = mysqli_fetch_assoc($get_Services)) {
+                // Format the price as a string with a dollar sign and two decimal places
+                $fetch_Services['Price'] = '$' . number_format((float) $fetch_Services['Price'], 2);
+
+                // Add the formatted fetch_Services array to your rows
                 $rows[] = $fetch_Services;
             }
+
+            // Now $rows contains all the data including formatted Prices
+
         } else {
             $rows = array("Unable To Access");
         }
